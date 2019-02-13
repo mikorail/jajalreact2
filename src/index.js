a64cdde0ae3af428e931219735dea10a98e58e5a
unxplainable <43263583+unxplainable@users.noreply.github.com>
2019-02-13 17:04:03 +0700

    pertemuan 2

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Product from './Product';
 import * as serviceWorker from './serviceWorker';
 
+/* const & elements */
+
 // function formatName(mahasiswa) {
-//     return mahasiswa.firstName + ' ' + mahasiswa.lastName;
+//     return mahasiswi.firstName + ' ' + mahasiswi.lastName;
 //   }
 
 //   function selamatDatang(mahasiswa){
-//       if(mahasiswa.angkatan < 2019){
-//           return "Selamat datang Maba " + mahasiswa.firstName;
+//       if(mahasiswi.angkatan < 2019){
+//           return "Selamat datang Maba " + mahasiswi.firstName;
 //       }
 //   }
   
-//   function formatNIM(mahasiswa) {
-//     return mahasiswa.nim;
+//   function formatNIM(mahasiswi) {
+//     return mahasiswi.nim;
 //   }
 
-//   function formatAngkatan(mahasiswa) {
-//     return mahasiswa.angkatan;
+//   function formatAngkatan(mahasiswi) {
+//     return mahasiswi.angkatan;
 //   }
   
-//   const mahasiswa = {
+//   const mahasiswi = {
 //     firstName: 'Harper',
 //     lastName: 'Perez',
 //     nim: 1234,
@@ -30,30 +33,37 @@ import * as serviceWorker from './serviceWorker';
 //   };
   
 //   const element = (
-//       <h1>{selamatDatang(mahasiswa)}</h1>
+//       <h1>{selamatDatang(mahasiswi)}</h1>
 //     <h1>
-//       Nama : {formatName(mahasiswa)}
+//       Nama : {formatName(mahasiswi)}
 //       <br></br>
-//       NIM : {formatNIM(mahasiswa)}
+//       NIM : {formatNIM(mahasiswi)}
 //       <br></br>
-//       Angkatan : {formatAngkatan(mahasiswa)}
+//       Angkatan : {formatAngkatan(mahasiswi)}
 //     </h1>
 //   );
   
 //   ReactDOM.render(element, document.getElementById('root'));
 
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-  }
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+//   }
   
-  setInterval(tick, 1000);
+//   setInterval(tick, 1000);
 
+/* functions & props */
+
+const element = <App/>;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+  // ReactDOM.render(<App/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
